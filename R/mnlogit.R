f896a722d9e06a48be7f4a9bf6a2468a2396c17e
xavi-rp <xavi.rotllan.puig@gmail.com>
2017-01-14 11:20:15 +0100

    - Changed minimum frequency of choice, in response, allowed (i.e. from 1e-7 to 1e-14)

--- R/mnlogit.R
+++ R/mnlogit.R
@@ -36,6 +36,7 @@ mnlogit <- function(formula, data, choiceVar=NULL, maxiter = 50, ftol = 1e-6,
              gtol = 1e-6, weights = NULL, ncores = 1, na.rm = TRUE, 
              print.level=0, linDepTol = 1e-6, start=NULL, alt.subset=NULL, ...)
 {
+    print("Using modified version of mnlogit...")
     startTime <- proc.time()[3]
     initcall <- match.call()    # Store original function call
 
@@ -149,10 +150,12 @@ mnlogit <- function(formula, data, choiceVar=NULL, maxiter = 50, ftol = 1e-6,
     loFreq <- min(freq.choices)
     loChoice <- choice.set[which(loFreq == freq.choices)]
     names(freq.choices) <- choice.set
-    if (loFreq < 1e-7) {
+    #xavi: if (loFreq < 1e-7) {
+    if (loFreq < 1e-14) {
         cat("Frequencies of alternatives in input data:\n")
         print(prop.table(freq.choices), digits = 4)
-        stop(paste("Frequency, in response, of choice:", loChoice, "< 1e-7."))
+        #xavi: stop(paste("Frequency, in response, of choice:", loChoice, "< 1e-7."))
+        stop(paste("Frequency, in response, of choice:", loChoice, "< 1e-14."))
     }
 
     # Form design matrices 
